d4cad9e589241030e5c6f4ea38f991d94af93f93
anproagile <anproagile@gmail.com>
2014-10-19 21:16:10 +0200

    Refactoring to ts

---
+++ WebMetaverse/WebMetaverse/pointerlock.ts
@@ -0,0 +1,103 @@
+﻿class PointerLock {
+
+    blocker: HTMLElement;
+    instructions: HTMLElement;
+    controls: any;
+
+    constructor(controls : any) {
+        this.blocker = document.getElementById('blocker');
+        this.instructions = document.getElementById('instructions');
+        this.controls = controls;
+        this.init();
+    }
+
+    init() {
+        
+        var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+        if (havePointerLock) {
+
+            var element = document.body;
+
+            var pointerlockchange = event => {
+
+                if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+
+                    this.controls.enabled = true;
+
+                    this.blocker.style.display = 'none';
+
+                } else {
+
+                    this.controls.enabled = false;
+
+                    this. blocker.style.display = '-webkit-box';
+                    this.blocker.style.display = '-moz-box';
+                    this.blocker.style.display = 'box';
+
+                    this.instructions.style.display = '';
+
+                }
+
+            }
+
+		    var pointerlockerror = function (event) {
+
+                this.instructions.style.display = '';
+
+            }
+
+		    // Hook pointer lock state change events
+		    document.addEventListener('pointerlockchange', pointerlockchange, false);
+            document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+            document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+            document.addEventListener('pointerlockerror', pointerlockerror, false);
+            document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+            document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+            this.instructions.addEventListener('click', e => {
+
+                this.instructions.style.display = 'none';
+
+                // Ask the browser to lock the pointer
+                element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+                if (/Firefox/i.test(navigator.userAgent)) {
+
+                    var fullscreenchange = function (event) {
+
+                        if (document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element) {
+
+                            document.removeEventListener('fullscreenchange', fullscreenchange);
+                            document.removeEventListener('mozfullscreenchange', fullscreenchange);
+
+                            element.requestPointerLock();
+                        }
+
+                    }
+
+		            document.addEventListener('fullscreenchange', fullscreenchange, false);
+                    document.addEventListener('mozfullscreenchange', fullscreenchange, false);
+
+                    element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+                    element.requestFullscreen();
+
+                } else {
+
+                    element.requestPointerLock();
+
+                }
+
+            }, false);
+
+        } else {
+
+            this.instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+
+        }
+    }
+
+
+}
