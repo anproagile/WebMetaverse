e10e54a14083f1af12f64d062cc1cfdbacb7ab75
anproagile <anproagile@gmail.com>
2014-11-15 19:36:05 +0100

    AvatarRoomMover

--- WebMetaverse/WebMetaverse/multi/avatarroommover.ts
+++ WebMetaverse/WebMetaverse/multi/avatarroommover.ts
@@ -1,9 +1,36 @@
 ﻿module wm.multi {
+
+    /**
+     * Moves avatars between rooms when a roomswitch packet is received.
+     */
     export class AvatarRoomMover {
 
+        roomCoordinator: verse.RoomCoordinator;
+        avatarWatcher: RemoteAvatarWatcher;
 
+        constructor(communicator: RoomCommunicator, coordinator: verse.RoomCoordinator, avatarWatcher: RemoteAvatarWatcher) {
+            this.roomCoordinator = coordinator;
+            this.avatarWatcher = avatarWatcher;
 
-        constructor() {
+            communicator.onRemoteUserRoomSwitch.add(this.moveAvatar);
+        }
+
+        moveAvatar = (from, to, userId) => {
+            var avatar = this.avatarWatcher.getAvatarForId(userId);
+            if (!avatar) {
+                throw "Trying to move non-existant avatar to some other room";
+            }
+
+            if (this.roomCoordinator.roomDictionary[from]) {
+                this.roomCoordinator.roomDictionary[from].remove(avatar.mesh);
+            }
+
+            if (this.roomCoordinator.roomDictionary[to]) {
+                this.roomCoordinator.roomDictionary[to].add(avatar.mesh);
+            }
+            else {
+                console.warn("Avatar moved to not yet loaded room, handling this is to be implemented");
+            }
 
         }
 
