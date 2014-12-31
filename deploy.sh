ae2a229ebf1f174d1da014e45fc66743319ab86b
anproagile <anproagile@gmail.com>
2014-12-31 02:48:16 +0100

    Lets not delete the built files..

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-rm -rf build|| exit 0;
-mkdir build
 cd site
-grunt
-( cd dist
+cd dist
  git init
  git config --global user.name "WM Bot"
  git config --global user.email "bot@webmetaverse.com"
  git add .
  git commit -m "Deploy to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
