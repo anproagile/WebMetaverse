04c7492ec5fbbc0e3543282acc0c708289adbe4b
anproagile <anproagile@gmail.com>
2015-05-31 22:56:56 +0200

    Deploy minimally without UI

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
-cd site
-cd dist
+#!/bin/bash
+rm -rf build|| exit 0;
+mkdir build; 
+grunt full
+( cd build
  git init
  git config --global user.name "WM Bot"
  git config --global user.email "bot@webmetaverse.com"
  git add .
  git commit -m "Deploy to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
